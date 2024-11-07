f24fa1b22451f4ac5ea2faccaa924a6a4f8368be
ivan <gopmiy36@gmail.com>
2024-11-07 23:50:35 +0300

    first

--- src/main/java/AllFiles/Controlers/PeopleControler.java
+++ src/main/java/AllFiles/Controlers/PeopleControler.java
@@ -1,4 +1,32 @@
 package AllFiles.Controlers;
 
+import AllFiles.dao.PersonDao;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/people")
 public class PeopleControler {
+
+    private final PersonDao personDao;
+
+    @Autowired
+    private PeopleControler(PersonDao personDao) {
+        this.personDao = personDao;
+    }
+
+    @GetMapping()// get all people
+    public String all(Model model) {
+        model.addAttribute("person",personDao.all());
+        return "/people/all";
+    }
+    @GetMapping("/{id}")//get 1 man from dao
+    public String user(@PathVariable("id") int id, Model model){
+        model.addAttribute("person",personDao.user(id));
+        return "/people/user";
+    }
 }
--- src/main/java/AllFiles/dao/PersonDao.java
+++ src/main/java/AllFiles/dao/PersonDao.java
@@ -1,4 +1,26 @@
 package AllFiles.dao;
 
+import AllFiles.models.Person;
+import org.springframework.stereotype.Component;
+import java.util.ArrayList;
+import java.util.List;
+
+@Component
 public class PersonDao {
+    private final List<Person> people;
+    private static int cntID;
+    {
+        people = new ArrayList<>();
+        people.add(new Person(++cntID,"John"));
+        people.add(new Person(++cntID,"Jane"));
+        people.add(new Person(++cntID,"Bob"));
+    }
+
+    public List<Person> all() {
+        return people;
+    }
+
+    public String user(int id){
+        return people.get(id).getName();
+    }
 }
